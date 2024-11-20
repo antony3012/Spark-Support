32645c1bc6fd90c3344e99f7c0d3fc3a57cce5f9
antony3012 <markantony3012@gmail.com>
2024-11-20 20:39:07 +0530

    first commit

---
+++ src/main/java/com/sparksupport/SparksupportApplication.java
@@ -0,0 +1,13 @@
+package com.sparksupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparksupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparksupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparksupport/SparksupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparksupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparksupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
